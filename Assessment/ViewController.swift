3a313deda907aa619cac4e970068f1f6294d029d
avidrush94 <avidrush94@gmail.com>
2023-08-13 10:15:16 +0530

    Initial Commit

---
+++ Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assessment
+//
+//  Created by Rushit on 13/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
